b1075d18e793f487e19ee0fc7fbb1a75a1011b55
yuanzhixiang <i@yuanzhixiang.com>
2022-03-20 17:49:35 +0800

    Modify group id and version

--- backtest-core/src/test/java/com/yuanzhixiang/bt/factor/common/FactorMaTest.java
+++ backtest-core/src/test/java/com/yuanzhixiang/bt/factor/common/FactorMaTest.java
@@ -1,73 +1,73 @@
-package com.yuanzhixiang.bt.factor.common;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import com.yuanzhixiang.bt.domain.model.valobj.Symbol;
-import com.yuanzhixiang.bt.engine.Application;
-import com.yuanzhixiang.bt.engine.Configuration;
-import com.yuanzhixiang.bt.factor.common.FactorMa.Ma;
-
-/**
- * @author yuanzhixiang
- */
-public class FactorMaTest  {
-
-    /**
-     * Verify the correctness of the price before restoration and the average price after restoration.
-     */
-    @Test
-    public void _0001() {
-        Configuration configuration = new Configuration();
-//        configuration.setDataSource(new DataSourceStockDaily());
-        configuration.registerFactor(new FactorMa(5, 10, 20, 60));
-        configuration.setTimeRange(
-            LocalDateTime.of(2021, 1, 1, 0, 0),
-            LocalDateTime.of(2021, 10, 30, 0, 0)
-        );
-        configuration.setSymbolList(List.of(
-            new Symbol("601919")
-        ));
-        new Application(configuration).run((context, factors) -> {
-            if (LocalDate.of(2021, 1, 5).equals(factors.getTradeDate().toLocalDate())) {
-                Ma ma = FactorMa.get(factors, 0);
-                Assert.assertEquals(12.64, ma.getMa(5), 0);
-                Assert.assertEquals(11.7, ma.getMa(10), 0);
-                Assert.assertEquals(10.62, ma.getMa(20), 0);
-                Assert.assertEquals(8.8, ma.getMa(60), 0);
-            }
-
-            // The restoration factor changes on the day.
-            if (LocalDate.of(2021, 9, 30).equals(factors.getTradeDate().toLocalDate())) {
-                Ma ma = FactorMa.get(factors, 0);
-                Assert.assertEquals(18.57, ma.getMa(5), 0);
-                Assert.assertEquals(19.76, ma.getMa(10), 0);
-                Assert.assertEquals(20.44, ma.getMa(20), 0);
-                Assert.assertEquals(20.77, ma.getMa(60), 0);
-            }
-        });
-    }
-
-    /**
-     * Verification factor duplicate registration.
-     */
-    @Test
-    public void _0002() {
-        Configuration configuration = new Configuration();
-        configuration.registerFactor(new FactorMa(5, 10, 20, 60));
-        configuration.registerFactor(new FactorMa(5));
-        Collection<Factor<?>> factors = configuration.getAllFactor();
-        Assert.assertEquals(1, factors.size());
-        Assert.assertEquals(4, ((FactorMa) factors.toArray()[0]).getCycleSet().size());
-
-        configuration.registerFactor(new FactorMa(120));
-        Assert.assertEquals(1, factors.size());
-        Assert.assertEquals(5, ((FactorMa) factors.toArray()[0]).getCycleSet().size());
-    }
-}
-
+//package com.yuanzhixiang.bt.factor.common;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.Collection;
+//import java.util.List;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import com.yuanzhixiang.bt.domain.model.valobj.Symbol;
+//import com.yuanzhixiang.bt.engine.Application;
+//import com.yuanzhixiang.bt.engine.Configuration;
+//import com.yuanzhixiang.bt.factor.common.FactorMa.Ma;
+//
+///**
+// * @author yuanzhixiang
+// */
+//public class FactorMaTest  {
+//
+//    /**
+//     * Verify the correctness of the price before restoration and the average price after restoration.
+//     */
+//    @Test
+//    public void _0001() {
+//        Configuration configuration = new Configuration();
+////        configuration.setDataSource(new DataSourceStockDaily());
+//        configuration.registerFactor(new FactorMa(5, 10, 20, 60));
+//        configuration.setTimeRange(
+//            LocalDateTime.of(2021, 1, 1, 0, 0),
+//            LocalDateTime.of(2021, 10, 30, 0, 0)
+//        );
+//        configuration.setSymbolList(List.of(
+//            new Symbol("601919")
+//        ));
+//        new Application(configuration).run((context, factors) -> {
+//            if (LocalDate.of(2021, 1, 5).equals(factors.getTradeDate().toLocalDate())) {
+//                Ma ma = FactorMa.get(factors, 0);
+//                Assert.assertEquals(12.64, ma.getMa(5), 0);
+//                Assert.assertEquals(11.7, ma.getMa(10), 0);
+//                Assert.assertEquals(10.62, ma.getMa(20), 0);
+//                Assert.assertEquals(8.8, ma.getMa(60), 0);
+//            }
+//
+//            // The restoration factor changes on the day.
+//            if (LocalDate.of(2021, 9, 30).equals(factors.getTradeDate().toLocalDate())) {
+//                Ma ma = FactorMa.get(factors, 0);
+//                Assert.assertEquals(18.57, ma.getMa(5), 0);
+//                Assert.assertEquals(19.76, ma.getMa(10), 0);
+//                Assert.assertEquals(20.44, ma.getMa(20), 0);
+//                Assert.assertEquals(20.77, ma.getMa(60), 0);
+//            }
+//        });
+//    }
+//
+//    /**
+//     * Verification factor duplicate registration.
+//     */
+//    @Test
+//    public void _0002() {
+//        Configuration configuration = new Configuration();
+//        configuration.registerFactor(new FactorMa(5, 10, 20, 60));
+//        configuration.registerFactor(new FactorMa(5));
+//        Collection<Factor<?>> factors = configuration.getAllFactor();
+//        Assert.assertEquals(1, factors.size());
+//        Assert.assertEquals(4, ((FactorMa) factors.toArray()[0]).getCycleSet().size());
+//
+//        configuration.registerFactor(new FactorMa(120));
+//        Assert.assertEquals(1, factors.size());
+//        Assert.assertEquals(5, ((FactorMa) factors.toArray()[0]).getCycleSet().size());
+//    }
+//}
+//
--- backtest-core/src/test/java/com/yuanzhixiang/bt/factor/common/RealPriceFactorTest.java
+++ backtest-core/src/test/java/com/yuanzhixiang/bt/factor/common/RealPriceFactorTest.java
@@ -1,80 +1,80 @@
-package com.yuanzhixiang.bt.factor.common;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.junit.Test;
-
-import com.yuanzhixiang.bt.domain.model.valobj.Symbol;
-import com.yuanzhixiang.bt.engine.Application;
-import com.yuanzhixiang.bt.engine.Configuration;
-import com.yuanzhixiang.bt.factor.common.RealPriceFactor.RealPrice;
-
-/**
- * @author yuanzhixiang
- */
-public class RealPriceFactorTest {
-
-    /**
-     * Verify that the real price is correct.
-     */
-    @Test
-    public void _0001() {
-        Configuration configuration = new Configuration();
-//        configuration.setDataSource(new DataSourceStockDaily());
-        configuration.setTimeRange(
-            LocalDateTime.of(2021, 7, 12, 0, 0),
-            LocalDateTime.of(2021, 7, 30, 0, 0)
-        );
-        configuration.setSymbolList(List.of(
-            new Symbol("601919")
-        ));
-        new Application(configuration).run((context, factors) -> {
-            if (LocalDate.of(2021, 7, 13).equals(factors.getTradeDate().toLocalDate())) {
-                // Verify original price
-                assertEquals(29.64, factors.getOpen().getPrice(), 0);
-                assertEquals(30.88, factors.getClose().getPrice(), 0);
-                assertEquals(31.04, factors.getHigh().getPrice(), 0);
-                assertEquals(29.31, factors.getLow().getPrice(), 0);
-
-                // Verify real price
-                RealPrice realPrice = RealPriceFactor.get(factors, 0);
-                assertNotNull(realPrice);
-                assertEquals(29.64, realPrice.getOpen(), 0);
-                assertEquals(30.88, realPrice.getClose(), 0);
-                assertEquals(31.04, realPrice.getHigh(), 0);
-                assertEquals(29.31, realPrice.getLow(), 0);
-            }
-
-            // The restoration factor changes on the day.
-            if (LocalDate.of(2021, 7, 14).equals(factors.getTradeDate().toLocalDate())) {
-                // Verify original price
-                assertEquals(24, factors.getOpen().getPrice(), 0);
-                assertEquals(23.16, factors.getClose().getPrice(), 0);
-                assertEquals(24.15, factors.getHigh().getPrice(), 0);
-                assertEquals(22.85, factors.getLow().getPrice(), 0);
-
-                // Verify real price
-                RealPrice realPrice = RealPriceFactor.get(factors, 0);
-                assertNotNull(realPrice);
-                assertEquals(24, realPrice.getOpen(), 0);
-                assertEquals(23.16, realPrice.getClose(), 0);
-                assertEquals(24.15, realPrice.getHigh(), 0);
-                assertEquals(22.85, realPrice.getLow(), 0);
-
-                // Verify real price
-                RealPrice pRealPrice = RealPriceFactor.get(factors, -1);
-                assertNotNull(pRealPrice);
-                assertEquals(22.8, pRealPrice.getOpen(), 0);
-                assertEquals(23.75, pRealPrice.getClose(), 0);
-                assertEquals(23.88, pRealPrice.getHigh(), 0);
-                assertEquals(22.55, pRealPrice.getLow(), 0);
-            }
-        });
-    }
-
-}
+//package com.yuanzhixiang.bt.factor.common;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//import org.junit.Test;
+//
+//import com.yuanzhixiang.bt.domain.model.valobj.Symbol;
+//import com.yuanzhixiang.bt.engine.Application;
+//import com.yuanzhixiang.bt.engine.Configuration;
+//import com.yuanzhixiang.bt.factor.common.RealPriceFactor.RealPrice;
+//
+///**
+// * @author yuanzhixiang
+// */
+//public class RealPriceFactorTest {
+//
+//    /**
+//     * Verify that the real price is correct.
+//     */
+//    @Test
+//    public void _0001() {
+//        Configuration configuration = new Configuration();
+////        configuration.setDataSource(new DataSourceStockDaily());
+//        configuration.setTimeRange(
+//            LocalDateTime.of(2021, 7, 12, 0, 0),
+//            LocalDateTime.of(2021, 7, 30, 0, 0)
+//        );
+//        configuration.setSymbolList(List.of(
+//            new Symbol("601919")
+//        ));
+//        new Application(configuration).run((context, factors) -> {
+//            if (LocalDate.of(2021, 7, 13).equals(factors.getTradeDate().toLocalDate())) {
+//                // Verify original price
+//                assertEquals(29.64, factors.getOpen().getPrice(), 0);
+//                assertEquals(30.88, factors.getClose().getPrice(), 0);
+//                assertEquals(31.04, factors.getHigh().getPrice(), 0);
+//                assertEquals(29.31, factors.getLow().getPrice(), 0);
+//
+//                // Verify real price
+//                RealPrice realPrice = RealPriceFactor.get(factors, 0);
+//                assertNotNull(realPrice);
+//                assertEquals(29.64, realPrice.getOpen(), 0);
+//                assertEquals(30.88, realPrice.getClose(), 0);
+//                assertEquals(31.04, realPrice.getHigh(), 0);
+//                assertEquals(29.31, realPrice.getLow(), 0);
+//            }
+//
+//            // The restoration factor changes on the day.
+//            if (LocalDate.of(2021, 7, 14).equals(factors.getTradeDate().toLocalDate())) {
+//                // Verify original price
+//                assertEquals(24, factors.getOpen().getPrice(), 0);
+//                assertEquals(23.16, factors.getClose().getPrice(), 0);
+//                assertEquals(24.15, factors.getHigh().getPrice(), 0);
+//                assertEquals(22.85, factors.getLow().getPrice(), 0);
+//
+//                // Verify real price
+//                RealPrice realPrice = RealPriceFactor.get(factors, 0);
+//                assertNotNull(realPrice);
+//                assertEquals(24, realPrice.getOpen(), 0);
+//                assertEquals(23.16, realPrice.getClose(), 0);
+//                assertEquals(24.15, realPrice.getHigh(), 0);
+//                assertEquals(22.85, realPrice.getLow(), 0);
+//
+//                // Verify real price
+//                RealPrice pRealPrice = RealPriceFactor.get(factors, -1);
+//                assertNotNull(pRealPrice);
+//                assertEquals(22.8, pRealPrice.getOpen(), 0);
+//                assertEquals(23.75, pRealPrice.getClose(), 0);
+//                assertEquals(23.88, pRealPrice.getHigh(), 0);
+//                assertEquals(22.55, pRealPrice.getLow(), 0);
+//            }
+//        });
+//    }
+//
+//}
